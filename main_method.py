943214664fe1aaafd6a4c08d9427f013b1e7c5af
aninair1905 <46573631+aninair1905@users.noreply.github.com>
2019-08-18 13:01:31 -0500

    Add files via upload

---
+++ main_method.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import argparse
+
+from pose import pose_estimation
+from split_vid import vid_into_frames
+
+
+parser = argparse.ArgumentParser('Pose estimation on ')
+parser.add_argument('--split', action='store_true', default=False, help='Split videos')
+parser.add_argument('--pose', action='store_true', default=False, help='Pose estimation')
+
+
+def main():
+    # root directory
+    root_direct = ''
+    # UCF_Anomalies dataset path
+    dset_root = os.path.join(root_direct, 'Videos')
+    args = parser.parse_args()
+
+    out_split = os.path.join(root_direct, 'train')
+    if not os.path.isdir(out_split):
+        os.mkdir(out_split)
+
+    # create new directory structure and write frames at path out_split
+    if args.split:
+        vid_into_frames(dset_root, out_split)
+
+
+    # output annotation path
+    outanns = os.path.join(root_direct, 'train_anns')
+    if not os.path.isdir(outanns):
+        os.mkdir(outanns)
+
+    # set working directory for AlphaPose framework
+    os.chdir('C:/Users/naira/PycharmProjects/Alphapose')
+    # create new directory structure and write annotation files at out_anns
+    if args.pose:
+        print('Running pose estimation: \n')
+        pose_estimation(out_split, outanns)
+
+if __name__ == "__main__":
+    main()
---
+++ split_vid.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+
+def vid_into_frames(dset_root, out_path):
+
+    for i, (folders, fil, directory) in enumerate(os.walk(dset_root)):
+        for vid_name in fil:
+
+            # path to video
+            vid_path = os.path.join(directory, vid_name)
+
+            # create a folder for each video
+            fname = vid_name.split('.mp4')[0]
+            path_fold = os.path.join(out_path, directory.split('/')[-1], fname)
+            if not os.path.isdir(path_fold):
+                os.makedirs(path_fold)
+
+            # executing ffmpeg -i file.mp4 -vf fps=5 path/%04d.jpg
+            print(path_fold)
+            cmd = "ffmpeg -i {}  -vf fps=5  {}/%04d.jpg".format(vid_path, path_fold)
+            subprocess.call(cmd, shell=True)
+
