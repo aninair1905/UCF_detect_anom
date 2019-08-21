475cc70ded2ef9df5f43a41e6b27b121e415b6d8
aninair1905 <46573631+aninair1905@users.noreply.github.com>
2019-08-20 22:29:13 -0500

    Add files via upload

---
+++ pose.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+
+def pose_estimation(outpath, outanns):
+
+    for i,(dire, folds, fils) in enumerate(os.walk(outpath)):
+        # run command on the frames
+        if len(folds) == 0 and len(fils) > 0:
+            # loop over video frames
+            print('Processing video frames in: {}'.format(dire))
+
+            # run a python module and output the json file at the corresponding annotation folder
+            ldir = dire.split('/')
+            out_json = os.path.join(outanns, ldir[-2], ldir[-1])
+            if not os.path.isdir(out_json):
+                os.makedirs(out_json)
+            cmd = "python demo.py --indir {}  --outdir {}".format(dire, out_json)
+            subprocess.call(cmd , shell=True)
